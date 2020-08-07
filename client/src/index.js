a590d873049d711bfc56876bd16e72f9d338689e
jenny ma <jenny_lau_8@hotmail.com>
2020-08-06 23:47:30 -0400

    import all components in index.js

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
+import Bookmarks from "./Bookmarks";
+import HomeFeed from "./HomeFeed";
+import Notifications from "./Notifications";
+import Profile from "./Profile";
+import TweetDetails from "./TweetDetails";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Bookmarks />
+    <HomeFeed />
+    <Notifications />
+    <Profile />
+    <TweetDetails />
   </React.StrictMode>,
   document.getElementById("root")
 );
