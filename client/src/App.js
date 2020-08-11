17b7d32c19fab54502a907565bd9af204a24c217
jenny ma <jenny_lau_8@hotmail.com>
2020-08-10 21:53:40 -0400

    added Switch

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const App = () => {
-  return <div>Hello world</div>;
+  return <div>Hello World from App.js</div>;
 };
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Route, Link, BrowserRouter as Router } from "react-router-dom";
+import { Route, Link, BrowserRouter as Router, Switch } from "react-router-dom";
 
 import App from "./App";
 import Bookmarks from "./Bookmarks";
@@ -13,12 +13,14 @@ ReactDOM.render(
   <React.StrictMode>
     <Router>
       <div>
-        <Route path="/" component={App} />
-        <Route path="/Bookmarks" component={Bookmarks} />
-        <Route path="/HomeFeed" component={HomeFeed} />
-        <Route path="/Notifications" component={Notifications} />
-        <Route path="/Profile" component={Profile} />
-        <Route path="/TweetDetails" component={TweetDetails} />
+        <Switch>
+          <Route exact path="/" component={App} />
+          <Route path="/Bookmarks" component={Bookmarks} />
+          <Route path="/HomeFeed" component={HomeFeed} />
+          <Route path="/Notifications" component={Notifications} />
+          <Route path="/Profile" component={Profile} />
+          <Route path="/TweetDetails" component={TweetDetails} />
+        </Switch>
       </div>
     </Router>
   </React.StrictMode>,
